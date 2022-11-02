884651421a97101528775534fa81287d61e0f79b
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-11-02 05:50:54 +0100

    great morning on props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
+import swim from './images/swimmer.png';
+import wed from './images/wedding.png';
 function App() {
   return (
     <div className="App">
     <Navbar />
     <Hero />
-    <Card />
+    <Card img={swim} />
+    <Card img={wed} />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,11 @@
-import swim from './images/swimmer.png';
 import {FaStar} from "react-icons/fa"
 
-const Card = () => {
+const Card = (props) => {
 
 
     return ( 
         <div className="card">
-             <img src={swim} className='card_image' alt='swimmer' />
+             <img src={props.img} alt='swimmer' />
             <span><FaStar color='gold' /> 5.0 (6)USA</span>
             <p>Life lessons with Katie Zaferes</p>
             <p><strong>From $136</strong> / person</p>
